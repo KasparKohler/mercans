ba5e502c20b0a18194fda125a5ed1c0194dc685f
Kaspar Kohler <kaspar.kohler@nortal.com>
2024-06-10 15:35:38 +0300

    create succesful login and session activate methods for APIs. Upgrade API tests.

--- pageObjects/API/loginAPI.ts
+++ pageObjects/API/loginAPI.ts
@@ -1,4 +1,4 @@
-import { APIRequestContext } from "@playwright/test"
+import { APIRequestContext, expect } from "@playwright/test"
 
 export default class LoginAPI {
     readonly request: APIRequestContext;
@@ -27,10 +27,26 @@ export default class LoginAPI {
     }
 
     /**
+     * [/login/credential] Log in with username and password.
+     * @param username 
+     * @param password 
+     * @returns Promise<any>
+     */
+    async logInWithUsernameAndPassword(username: string, password: string) {
+        console.log('Log in with username and password.')
+        const postUserCredentialsResponse = await this.postUserCredentials(username, password)
+        const postUserCredentialsResponseBody = await postUserCredentialsResponse.json()
+        expect(postUserCredentialsResponse.status()).toEqual(200);
+        expect(postUserCredentialsResponseBody.credentialToken).toBeTruthy()
+
+        return postUserCredentialsResponseBody
+    }
+
+    /**
      * [/login/session] POST request.
      * @param credentialToken 
      * @param employeeId 
-     * @returns 
+     * @returns apiResponse Promise<APIResponse>
      */
     async postSession(credentialToken: string, employeeId: number) {
         console.log('[/login/session] POST request.')
@@ -44,5 +60,35 @@ export default class LoginAPI {
                 "Authorization": `Basic ${process.env.NODE_BASIC_AUTH_TOKEN}`
             }
         })
+    }
+
+    /**
+     * [/login] Activate user session.
+     * @param credentialToken 
+     * @param employeeId 
+     * @returns Promise<any>
+     */
+    async activateSession(credentialToken: string, employeeId: number) {
+        console.log('Activate user session.')
+        const sessionResponse = await this.postSession(credentialToken, employeeId)
+        const sessionResponseBody = await sessionResponse.json()
+        expect(sessionResponse.status()).toEqual(200);
+        expect(sessionResponseBody.sessionToken).toBeTruthy()
+
+        return sessionResponseBody
+    }
+
+    /**
+     * Log in with username and password and activate session.
+     * @param username 
+     * @param password 
+     * @returns sessionToken
+     */
+    async loginWithUsernameAndPasswordAndActivateSession(username: string, password: string) {
+        console.log('Log in with username and password and activate session.')
+        const loginResponseBody = await this.logInWithUsernameAndPassword(username, password)
+        const sessionResponseBody = await this.activateSession(loginResponseBody.credentialToken, loginResponseBody.employees[0]["id"])
+        
+        return sessionResponseBody.sessionToken
     }
 }
--- tests/apitests.spec.ts
+++ tests/apitests.spec.ts
@@ -3,29 +3,24 @@ import EmployeeAPI from "../pageObjects/API/employeeAPI";
 import LoginAPI from "../pageObjects/API/loginAPI";
 import Employees from "../testData/employees";
 
-const employeeId = 99999
-
 test('It is not possible to login with invalid password', async ({ request }) => {
   const loginAPI = new LoginAPI(request)
   const employeeAPI = new EmployeeAPI(request);
+
   const loginResponse = await loginAPI.postUserCredentials(Employees.manager.username, 'kartul');
   const loginResponseBody = await loginResponse.json()
   expect(loginResponse.status()).toEqual(401);
   expect(loginResponseBody.errors[0]["code"]).toEqual("invalid_username_or_password")
-  const employeeResponse = await employeeAPI.getEmployeeInfo("")
 
+  const employeeResponse = await employeeAPI.getEmployeeInfo("")
   expect(employeeResponse.status()).toEqual(401)
 });
 
-test('It is not possible to activate session with other invalid employee id', async ({request}) => {
+test('It is not possible to activate session with invalid employee id', async ({ request }) => {
   const loginAPI = new LoginAPI(request)
-  const loginResponse = await loginAPI.postUserCredentials(Employees.manager.username, Employees.manager.password);
-  
-  const loginResponseBody = await loginResponse.json()
-  expect(loginResponse.status()).toEqual(200);
-  expect(loginResponseBody.credentialToken).toBeTruthy()
-  
-  const sessionResponse = await loginAPI.postSession(loginResponseBody.credentialToken, employeeId)
+  const loginResponseBody = await loginAPI.logInWithUsernameAndPassword(Employees.manager.username, Employees.manager.password)
+
+  const sessionResponse = await loginAPI.postSession(loginResponseBody.credentialToken, 99999)
   const sessionResponseBody = await sessionResponse.json()
   expect(sessionResponse.status()).toEqual(401);
   expect(sessionResponseBody.errors[0]["code"]).toEqual("invalid_credential_token_or_client_id")
@@ -34,17 +29,9 @@ test('It is not possible to activate session with other invalid employee id', as
 test('It is possible to login with valid username and password', async ({ request }) => {
   const loginAPI = new LoginAPI(request)
   const employeeAPI = new EmployeeAPI(request);
-  const loginResponse = await loginAPI.postUserCredentials(Employees.manager.username, Employees.manager.password);
-  
-  const loginResponseBody = await loginResponse.json()
-  expect(loginResponse.status()).toEqual(200);
-  expect(await loginResponseBody.credentialToken).toBeTruthy()
-  
-  const sessionResponse = await loginAPI.postSession(await loginResponseBody.credentialToken, await loginResponseBody.employees[0]["id"])
-  const sessionResponseBody = await sessionResponse.json()
-  expect(sessionResponse.status()).toEqual(200);
-  
-  const employeeResponse = await employeeAPI.getEmployeeInfo(sessionResponseBody.sessionToken)
+  const sessionToken = await loginAPI.loginWithUsernameAndPasswordAndActivateSession(Employees.manager.username, Employees.manager.password)
+
+  const employeeResponse = await employeeAPI.getEmployeeInfo(sessionToken)
   const employeeResponseBody = await employeeResponse.json()
   expect(employeeResponse.status()).toEqual(200)
   expect(employeeResponseBody.person.firstName).toEqual(Employees.manager.name.split(' ')[0])
